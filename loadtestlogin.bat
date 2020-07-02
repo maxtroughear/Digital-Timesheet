c888fe7a1f6b1f2f01ce1932dfc5c4139e5d4889
Max Troughear <max@maxtroughear.dev>
2020-07-02 16:26:57 +1200

    Load Test update

--- loadtestlogin.bat
+++ loadtestlogin.bat
@@ -1,3 +1,3 @@
 @ECHO off
-CALL go-wrk.exe -M POST -T 10000 -c 20 -d 60 -H "Content-Type: application/json" -body @testlogin.json "http://localhost/api/"
+CALL go-wrk.exe -M POST -T 10000 -c 10 -d 5 -H "Content-Type: application/json" -body @testlogin.json "http://localhost/api/"
 PAUSE
