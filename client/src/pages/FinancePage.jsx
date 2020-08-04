2e747c615ae7da4258f388dda77e2e5fcc9cd283
Max Troughear <max@maxtroughear.dev>
2020-08-04 14:15:59 +1200

    Extending on lazy/suspense loading

--- client/src/pages/FinancePage.jsx
+++ client/src/pages/FinancePage.jsx
@@ -1,19 +1,22 @@
 /** @jsx jsx */
 /** @jsxFrag React.Fragment */
 import { jsx } from '@emotion/core';
-import { lazy } from 'react';
+import { Suspense, lazy } from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { FullPanelSpinner } from 'components/lib';
 import NotFoundPage from './NotFound';
 
 const FinanceDashboard = lazy(() => import('./finance/Dashboard'));
 const ClientsDashboard = lazy(() => import('./finance/Clients'));
 
 const FinancePage = () => (
-  <Switch>
-    <Route path="/finance" exact component={FinanceDashboard} />
-    <Route path="/finance/clients" component={ClientsDashboard} />
-    <Route path="*" component={NotFoundPage} />
-  </Switch>
+  <Suspense fallback={<FullPanelSpinner />}>
+    <Switch>
+      <Route path="/finance" exact component={FinanceDashboard} />
+      <Route path="/finance/clients" component={ClientsDashboard} />
+      <Route path="*" component={NotFoundPage} />
+    </Switch>
+  </Suspense>
 );
 
 export default FinancePage;
